6751d83a43159ec34080d3594eb4f5abd4e50f44
Lorenzo Fernandez <lorenzotomasf@gmail.com>
2021-11-01 23:17:21 -0300

    variables

---
+++ js/script.js
@@ -0,0 +1,18 @@
+alert("¡Hola! Antes de avanzar necesitas completar ciertos datos");
+// Una alerta en la cual aclara que se necesitan completar ciertos datos para poder seguir en la pagina web
+
+const nombreYApellido = prompt("Ingrese su nombre y apellido");
+let edad = prompt("Ingrese su edad");
+const documento = prompt("Ingrese su numero de documento");
+// Datos del usuario
+
+alert("Necesitamos verificar que es un humano. Por favor realice los siguientes calculos.")
+// Una alerta para corroborar que la persona que visita la pagina es un ser humano
+
+let numero = prompt("Ingresar un numero");
+    numero2 = prompt("Ingresar otro numero");
+// Ingresar dos numeros cualquiera para  poder realizar una suma
+
+let verificacionHumano = prompt("La suma de "+ numero + " y "+ numero2 + " es: ");
+// Se realiza una suma de los numeros elegidos anteriormente
+
