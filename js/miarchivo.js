ad5cd13f37409988c603b426a29f87e4b3292eca
Lorenzo Fernandez <lorenzotomasf@gmail.com>
2021-11-18 22:28:26 -0300

    Corrigiendo

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -2,108 +2,108 @@
 
 alert ( "¡Hola! Antes de avanzar necesitas completar ciertos datos" ) ;
 
-// Una alerta en la cual aclara que se necesitan completar ciertos datos para poder seguir en la pagina web
+// Un alert en la cual aclara que se necesitan completar ciertos datos para poder seguir en la pagina web
 
-const  nombreYA apellido  =  prompt ( "Ingrese su nombre y apellido" ) ;
+const  nombreYApellido  =  prompt ( "Ingrese su nombre y apellido" ) ;
 let  edad  =  prompt ( "Ingrese su edad" ) ;
 const  documento  =  prompt ( "Ingrese su numero de documento" ) ;
 // Datos del usuario
 
 alert ( "Necesitamos verificar que es un humano. Por favor realizar los siguientes calculos." )
-// Una alerta para corroborar que la persona que visita la pagina es un ser humano
+// Un alert para corroborar que la persona que visita la pagina es un ser humano
 
-let  numero  =  prompt ( "Ingresar un numero" ) ;
-    numero2  =  prompt ( "Ingresar otro numero" ) ;
+let  numero  =  prompt ( "Ingresar un numero: " ) ;
+    numero2  =  prompt ( "Ingresar otro numero: " ) ;
 // Ingresar dos numeros cualquiera para poder realizar una suma
 
-let  verificacionHumano  =  prompt ( "La suma de" +  numero  +  "y" +  numero2  +  "es:" ) ;
+let  verificacionHumano  =  prompt ( "La suma de " +  numero  +  " y " +  numero2  +  " es: " ) ;
 // Se realiza una suma de los numeros elegidos anteriormente
 
 
 // Desafio "Crear algoritmo con un condicional"
 
-let  numero3  =  parseFloat ( prompt ( "Ingrese otro numero" ) ) ;
+let  numero3  =  parseFloat ( prompt ( "Ingrese otro numero" )) ;
 // Se pide ingresar cualquier numero
 
 if ( numero3 > 10  &&  numero3 < 50 ) {
-    alerta ( "El numero esta entre 10 y 50" )
+    alert ( "El numero esta entre 10 y 50" )
 }
-// En caso de que el numero este entre 10 y 50 se mostrara una alerta
+// En caso de que el numero este entre 10 y 50 se mostrara una alert
 
 // Desafio "Crear un algoritmo utilizando un ciclo"
 
 let  numero4  =  parseInt ( prompt ( "Ingresar un numero entero:" ) ) ;
 // Se crea una variable en el cual el usuario debe ingresar un numero entero 
-para  ( sea  i  =  0 ;  i  <  numero4  ;  i ++ )  {
-    alert ( "Hola" ) ;
+for (let i=0; i<numero4;i ++)  {
+    alert ("Hola") ;
 }
 
 // Desafio "Simulador interactivo"
 
-función  producto ( precio )  {
+function  producto (precio)  {
 
-    deje  descuento  =  0.50 ;
+    let descuento  =  0.50 ;
     
-    let  precioDelProducto  =  parseFloat ( precio  *  descuento ) ;
+    let  precioDelProducto  =  parseFloat( precio*descuento) ;
   
     return  precioDelProducto ;
 
 }
 
-dejemos  resultado  =  producto ( "400" ) ;
+let resultado  =  producto ("400");
 // 400 seria el parametro precio
 
-alerta ( resultado )
+alert(resultado)
 // Desafio complementario "Funciones relacionadas"
 
 let  precio  =  parseInt ( prompt ( "Ingrese el precio del producto:" ) )
-sea  impuesto  =  0.21 ;
+let  impuesto  =  0.21 ;
 
-function  precioDelProducto ( precio )  {
-    if  ( precio  ! =  0  &&  precio  ! =  "" )  {
-        alert ( "El precio ingresado es:"  +  precio ) ;
+function  precioDelProducto (precio )  {
+    if  ( precio  !=  0  &&  precio  !=  "" )  {
+        alert ( "El precio ingresado es: "  +  precio ) ;
     }
 }
 
 function  sumarImpuesto ( precio )  {
-    alerta ( "Debemos sumarle el 21% de impuestos" )
+    alert ( "Debemos sumarle el 21% de impuestos" )
     let  precioConImpuesto  =  parseFloat ( precio  *  impuesto ) ;
     return  precioConImpuesto ;
 }
 
 function  precioFinal ( valorFinal )  {
-    alert ( "El precio final es:"  +  valorFinal ) ;
+    alert ( "El precio final es: "  +  valorFinal ) ;
 }
 
 precioDelProducto ( precio ) ;
 let  precioConImpuesto  =  sumarImpuesto ( precio ) ;
-precioFinal ( precioConImpuesto ) ;
+precioFinal(precioConImpuesto);
 
 // Desafio Incorporar objetos
 
-class  Producto  {
+class  Producto{
     
     constructor ( marca ,  precio )  {
-        esto . marca  =  prompt ( "Ingresar el nombre del producto:" ) ;
-        esto . precio  =  parseFloat ( prompt ( "Ingrese el precio del producto:" ) ) ;
+        this.marca  =  prompt ( "Ingresar el nombre del producto: " ) ;
+        this.precio  =  parseFloat(prompt("Ingrese el precio del producto: "));
     }
-    impuesto ( ) {
-        devuelve  esto . precio  *  0,21 ;
+    impuesto(){
+        return  this.precio * 0,21 ;
     }
     
-    mostrar ( ) {
-        alert ( "Este es el producto de" + this . marca + "con el precio de: $" + this . impuesto ( ) ) ;
+    mostrar(){
+        alert( "Este es el producto de " + this.marca + " con el precio de: $" + this.impuesto()) ;
     }
 }
 
-const  producto1  =  nuevo  Producto ( ) ;
-producto1 . mostrar ( ) ;
+const  producto1  =  new  Producto();
+producto1.mostrar();
 
 // Desafio "Incorporar arrays"
 
 let  nombreDelProductoALlevar  =  prompt ( "Ingrese el nombre del producto a llevar:" )
-const  productoALlevar  =  [ nombreDelProductoALlevar ] ;
-alert ( "Va a llevar"  +  productoALlevar . longitud  +  "producto"  ) ; 
+const  productoALlevar  =  [nombreDelProductoALlevar] ;
+alert ( "Va a llevar "  +  productoALlevar.length +  " producto "  ) ; 
 
 // Desafio complementario "Ordenar un array de objetos"
 
@@ -122,4 +122,3 @@ function ordenar(a, b){
 }
 
 alert(preciosDeTodosLosProductos);
-
